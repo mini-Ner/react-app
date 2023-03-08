da1f22ebc492d53f891ca3c7e1feb4127ffb370a
ner <tjwor10@naver.com>
2023-03-08 15:14:39 +0900

    first test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(){
+  return  <header>
+  <h1><a href="/">WEB</a></h1>
+  </header> 
+}
+function Body(){
+  return <body>
+    <h2><a href="/">component</a></h2>
+  </body>
+}
+function Nav(){
+ return <nav>
+  <ol>
+  <li><a href="/read/1">html</a></li>
+  <li><a href="/read/2">css</a></li>
+  <li><a href="/read/3">js</a></li>
+</ol>
+</nav>
+}
+function Article(){
+return <article>
+    <h2>welcome</h2>
+        hello, WEB
+</article>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header></Header>
+     <Body></Body>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
